367530b223fb4429a4c6c598e7a83cc268f151df
Pavel Yudin <pyudin@parallels.com>
2015-05-25 19:57:54 +0300

    couple of integration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe 'bacula-client::default' do
-
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'installs bacula-client version 5.0.0' do
+    expect(package('bacula-client')).to be_installed.with_version('5.0.0')
   end
 
+  it 'creates bacula-client config' do
+    expect(file('/etc/bacula/bacula-fd.conf')).to be_file
+  end
 end
