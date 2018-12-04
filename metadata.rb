fa167855f32887aa291661b6d5bde3ab8e0a217c
Alexander Polivkin <apolivkin@parallels.com>
2018-12-04 17:09:02 +0300

    Version bumped to 1.1.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pyudin@parallels.com'
 license          'Apache 2.0'
 description      'Installs/Configures bacula-client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.4'
+version          '1.1.4'
 
 depends 'apt'
 depends 'build-essential'
